5639daf6e4dd3361934481ba5a980b1d379f076c
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-09-13 20:11:00 -0300

    Set up bff project

---
+++ src/test/java/com/bancoxyz/bff_atm/BffAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.bff_atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
